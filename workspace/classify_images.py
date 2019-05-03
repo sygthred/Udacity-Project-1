8fe7c22a08dc58775541258c185e17ea5b308942
Parigyan Tamuly <40274913+kornbolt@users.noreply.github.com>
2019-05-04 00:07:37 +0530

    Update classify_images.py

--- workspace/classify_images.py
+++ workspace/classify_images.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
 #                                                                             
-# PROGRAMMER: Tushar
-# DATE CREATED: 25-01-2019                            
+# PROGRAMMER: Parigyan Tamuly
+# DATE CREATED: 22/03/2019                      
 # REVISED DATE: 
 # PURPOSE: Create a function classify_images that uses the classifier function 
 #          to create the classifier labels and then compares the classifier 
