b36aa7270ea8a9efd0e597fcb25ada738317b9c3
Parigyan Tamuly <40274913+kornbolt@users.noreply.github.com>
2019-05-03 23:52:47 +0530

    Update adjust_results4_isadog_hints.py

--- workspace/adjust_results4_isadog_hints.py
+++ workspace/adjust_results4_isadog_hints.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog_hints.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
+# PROGRAMMER: Parigyan Tamuly
+# DATE CREATED: 22/03/2019                                
 # REVISED DATE: 
 # PURPOSE: This is a *hints* file to help guide students in creating the 
 #          function adjust_results4_isadog that adjusts the results dictionary
