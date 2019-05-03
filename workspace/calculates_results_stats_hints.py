f45f1d25fa075d4ffd8cc98975e3905ce8431b61
Parigyan Tamuly <40274913+kornbolt@users.noreply.github.com>
2019-05-04 00:05:43 +0530

    Update calculates_results_stats_hints.py

--- workspace/calculates_results_stats_hints.py
+++ workspace/calculates_results_stats_hints.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats_hints.py
 #                                                                             
-# PROGRAMMER:
-# DATE CREATED:                                  
+# PROGRAMMER: Parigyan Tamuly
+# DATE CREATED: 22/03/2019                                  
 # REVISED DATE: 
 # PURPOSE: This is a *hints* file to help guide students in creating the 
 #          function calculates_results_stats that calculates the statistics
