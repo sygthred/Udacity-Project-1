0660ee284132842a9f1af16b5b7418684c4ee195
Parigyan Tamuly <40274913+kornbolt@users.noreply.github.com>
2019-05-04 00:06:56 +0530

    Update check_images.py

--- workspace/check_images.py
+++ workspace/check_images.py
@@ -3,8 +3,8 @@
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: Tushar
-# DATE CREATED: 1-29-2019                                 
+# PROGRAMMER: Parigyan Tamuly
+# DATE CREATED: 22/03/2019                             
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
