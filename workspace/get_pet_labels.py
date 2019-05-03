edd48d931a60e1d3214a591a577ba6324c597cd0
Parigyan Tamuly <40274913+kornbolt@users.noreply.github.com>
2019-05-04 00:08:59 +0530

    Update get_pet_labels.py

--- workspace/get_pet_labels.py
+++ workspace/get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: Tushar
-# DATE CREATED:                                  
+# PROGRAMMER: Parigyan Tamuly
+# DATE CREATED: 22/03/2019                          
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
